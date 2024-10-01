5ea866ae59d82822a69fddf5d4e17425c7d989a6
eulerfernandes <pedraeuler@gmail.com>
2024-10-01 20:19:27 -0300

    Configurando banco de dados

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express"
+
+const routes = new Router()
+
+routes.get("/", (request, response) => {
+  return response.json({ message: "Hello Word" })
+})
+
+export default routes
