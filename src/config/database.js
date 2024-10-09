3e72156a3ee3f91aad6d4b0338706dabd75d5245
eulerfernandes <pedraeuler@gmail.com>
2024-10-09 04:29:05 -0300

    database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  usarname: "postgres",
+  password: "postgres",
+  database: "loja",
+  define: {
+    timepamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
