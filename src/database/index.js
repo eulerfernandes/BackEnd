b9e8366bc611fa2109870224e870b49cba7bfa8f
eulerfernandes <pedraeuler@gmail.com>
2024-10-15 05:23:43 -0300

    database

---
+++ src/database/index.js
@@ -0,0 +1,13 @@
+import Sequelize from 'sequelize'
+
+import User from '../app/models'
+
+class Database {
+  constructor() {
+    this.init()
+  }
+
+  init() {}
+}
+
+export defaultnew Database();
